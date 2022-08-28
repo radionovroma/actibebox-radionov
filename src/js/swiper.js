ecb83d9477918b530d021dc2511028423cf2b749
roman <r0m4uk0106@gmail.com>
2022-08-28 21:41:56 +0300

    Added advice block

---
+++ src/js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.advice-block__swiper', {
+	slidesPerView: 'auto',
+	centeredSlides: true,
+	initialSlide: 1,
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	}
+});
